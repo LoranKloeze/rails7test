5b2408b63eee1edeb52e357a3213e4a63b88b656
Loran Kloeze <loran@codedivision.nl>
2021-09-19 19:21:58 +0200

    Initial

---
+++ app/javascript/controllers/ui_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="ui"
+export default class extends Controller {
+  connect() {
+  }
+
+  doit() {
+    console.log('aaasssww')
+  }
+}
